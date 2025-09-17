992b0062456c24460a13c9642205de55b5a2614d
RaptorSDS <raptor.o2@hotmail.de>
2025-09-17 17:25:10 +0200

    Update configure_gps.sh
    Adding Unerline at ReciverName

--- configure_gps.sh
+++ configure_gps.sh
@@ -8,7 +8,7 @@
 BASEDIR="$(dirname "$0")"
 source <( grep -v '^#' "${BASEDIR}"/settings.conf | grep '=' ) #import settings
 
-if [[ "${receiver}" = "Quectel LC29HBS" ]]; then
+if [[ "${receiver}" = "Quectel_LC29HBS" ]]; then
   speed="${com_port_settings%%:*}"
   python3 "${BASEDIR}"/tools/nmea.py --file "${BASEDIR}"/receiver_cfg/LC29HBS_Configure.txt /dev/"${com_port}" "${speed}" 3
   python3 "${BASEDIR}"/tools/nmea.py --file "${BASEDIR}"/receiver_cfg/LC29HBS_Save.txt /dev/"${com_port}" "${speed}" 3
